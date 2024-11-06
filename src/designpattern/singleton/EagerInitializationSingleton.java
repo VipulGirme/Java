c1b51d75babe49a72bb5cfe7d5eefb9c87471c0f
Vipul R Girme <49040675+VipulGirme@users.noreply.github.com>
2024-11-06 18:46:52 +0530

    First Commit

--- src/designpattern/singleton/EagerInitializationSingleton.java
+++ src/designpattern/singleton/EagerInitializationSingleton.java
@@ -7,6 +7,14 @@ public class EagerInitializationSingleton {
     private EagerInitializationSingleton() {}
 
     public static EagerInitializationSingleton getInstance() {
-        return instance; 
+        return instance;
     }
+
+    /*
+     * If your singleton class is not using a lot of resources,
+     * this is the approach to use. But in most of the scenarios,
+     * singleton classes are created for resources such as File System, Database connections, etc.
+     * We should avoid the instantiation unless the client calls the getInstance method.
+     * Also, this method doesn’t provide any options for exception handling.
+     * */
 }
